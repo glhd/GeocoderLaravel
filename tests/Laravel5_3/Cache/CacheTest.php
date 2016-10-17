e00b800d58e8b04a67d6615385bf7f4da63db17d
Junior Grossi <juniorgro@gmail.com>
2016-10-17 16:03:32 -0200

    Change cache test case to test reverse() method too

--- tests/Laravel5_3/Cache/CacheTest.php
+++ tests/Laravel5_3/Cache/CacheTest.php
@@ -27,24 +27,43 @@ class CacheTest extends TestCase
             ->using('google_maps');
 
         $geocoder->setCache(app()->make('cache'));
-        $address = '1600 Pennsylvania Ave., Washington, DC USA';
 
-        // Disabling cache
-        config()->set('geocoder.cache.enabled', false);
-        $start = microtime(true);
-        $result = $geocoder->geocode($address)->all();
-        $end = microtime(true);
-        $timeWithoutCache = $end - $start;
+        $addresses = [
+            'geocode' => '1600 Pennsylvania Ave., Washington, DC USA', // By address
+            'reverse' => [40.026104, -75.225246], // By latitude and longitude
+        ];
 
-        // Enabling cache
-        config()->set('geocoder.cache.enabled', true);
-        $result = $geocoder->geocode($address)->all(); // Call first time to cache
-        $start = microtime(true);
-        $result = $geocoder->geocode($address)->all();
-        $end = microtime(true);
-        $timeWithCache = $end - $start;
+        foreach ($addresses as $method => $address) {
+            // Disabling cache
+            config()->set('geocoder.cache.enabled', false);
 
-        // Ensure that the time with cache is lower than 60% of the time without cache
-        $this->assertTrue($timeWithCache < ($timeWithoutCache * 0.6));
+            $start = microtime(true);
+            if ($method == 'geocode') {
+                $result = $geocoder->geocode($address)->all();
+            } else {
+                $result = $geocoder->reverse($address[0], $address[1])->all();
+            }
+            $end = microtime(true);
+            $timeWithoutCache = $end - $start;
+
+            // Enabling cache
+            config()->set('geocoder.cache.enabled', true);
+            if ($method == 'geocode') {
+                $result = $geocoder->geocode($address)->all(); // Call first time to cache
+            } else {
+                $result = $geocoder->reverse($address[0], $address[1])->all(); // Call first time to cache
+            }
+            $start = microtime(true);
+            if ($method == 'geocode') {
+                $result = $geocoder->geocode($address)->all();
+            } else {
+                $result = $geocoder->reverse($address[0], $address[1])->all();
+            }
+            $end = microtime(true);
+            $timeWithCache = $end - $start;
+
+            // Ensure that the time with cache is lower than 60% of the time without cache
+            $this->assertTrue($timeWithCache < ($timeWithoutCache * 0.6));
+        }
     }
 }
